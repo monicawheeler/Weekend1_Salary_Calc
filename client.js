25140f07e71f0abbb6d793cc7df081220cfc2d2f
Monica Wheeler <monica.c.wheeler@gmail.com>
2017-12-15 15:31:28 -0600

    Create pseudo code and create rough plan for application. Create base files

---
+++ client.js
@@ -0,0 +1,19 @@
+console.log('client.js is sourced');
+
+// Submit button takes the input values
+
+// Add Salaries to report monthly costs
+
+// Append values to DOM
+
+// Clear values from input fields
+
+// Calculate monthly costs and append to DOM
+
+
+// HARD MODE
+// Delete button removes employee from DOM
+
+
+// PRO MODE
+// After deleting employee, update total spend on salaries account
